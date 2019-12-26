7452e8a072e9959da504c5c8acb79346beebb6f3
SarlosCainz <m-takuj@sarlos.jp>
2019-12-26 13:28:39 +0900

    bug fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,10 @@ fi
 
 OPTIONS=
 if [ "x$PLANTUML_URL" != "x" ]; then
-  OPTIONS="--plantuml-url $PLANTUML_URL"
+  OPTIONS="$OPTIONS --plantuml-url $PLANTUML_URL"
 fi
 if [ "x$NO_EDIT" != "x" ]; then
-  OPTIONS="--no-edit"
+  OPTIONS="$OPTIONS --no-edit"
 fi
 
 exec gollum --port=80 --adapter=rugged $OPTIONS "$@"
